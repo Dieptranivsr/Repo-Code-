d84ac10e185f703505233db71e56550ef8b743f8
Diep Tran <69444682+Dieptranivsr@users.noreply.github.com>
2022-01-25 15:10:48 +0700

    upload roots of Cubic Equation

---
+++ CodeC&&Cplusplus/Cubic.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <eigen3/Eigen/Dense>
+
+std::vector<double> cubic(double a, double b, double c, double d) {
+  std::vector<double> dts;
+  
+  double a2 = b / a;
+  double a1 = c / a;
+  double a0 = d / a;
+
+  double Q = (3 * a1 - a2 * a2) / 9;
+  double R = (9 * a1 * a2 - 27 * a0 - 2 * a2 * a2 * a2) / 54;
+  double D = Q * Q * Q + R * R;
+  if (D > 0) {
+    double S = std::cbrt(R + sqrt(D));
+    double T = std::cbrt(R - sqrt(D));
+    dts.push_back(-a2 / 3 + (S + T));
+    return dts;
+  } else if (D == 0) {
+    double S = std::cbrt(R);
+    dts.push_back(-a2 / 3 + S + S);
+    dts.push_back(-a2 / 3 - S);
+    return dts;
+  } else {
+    double theta = acos(R / sqrt(-Q * Q * Q));
+    dts.push_back(2 * sqrt(-Q) * cos(theta / 3) - a2 / 3);
+    dts.push_back(2 * sqrt(-Q) * cos((theta + 2 * M_PI) / 3) - a2 / 3);
+    dts.push_back(2 * sqrt(-Q) * cos((theta + 4 * M_PI) / 3) - a2 / 3);
+    return dts;
+  }
+}
+
+int main(){
+  std::vector<double> root = cubic(1,2,6,4);
+  std::cout << "root of equation: " << root.at(0) << std::endl;
+}
