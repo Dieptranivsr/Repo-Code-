6fd6f410e28cddbf970d868c2f8755f231daf497
Diep Tran <69444682+Dieptranivsr@users.noreply.github.com>
2022-01-25 15:14:36 +0700

    Upload Roots of Quartic Equation

---
+++ CodeC&&Cplusplus/Quartic.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <eigen3/Eigen/Dense>
+
+std::vector<double> quartic(double a, double b, double c, double d, double e) {
+  std::vector<double> dts;
+
+  double a3 = b / a;
+  double a2 = c / a;
+  double a1 = d / a;
+  double a0 = e / a;
+
+  std::vector<double> ys = cubic(1, -a2, a1 * a3 - 4 * a0, 4 * a2 * a0 - a1 * a1 - a3 * a3 * a0);
+  double         y1 = ys.front();
+  double         r  = a3 * a3 / 4 - a2 + y1;
+  if (r < 0) return dts;
+
+  double R = sqrt(r);
+  double D, E;
+  if (R != 0) {
+    D = sqrt(0.75 * a3 * a3 - R * R - 2 * a2 + 0.25 * (4 * a3 * a2 - 8 * a1 - a3 * a3 * a3) / R);
+    E = sqrt(0.75 * a3 * a3 - R * R - 2 * a2 - 0.25 * (4 * a3 * a2 - 8 * a1 - a3 * a3 * a3) / R);
+  } else {
+    D = sqrt(0.75 * a3 * a3 - 2 * a2 + 2 * sqrt(y1 * y1 - 4 * a0));
+    E = sqrt(0.75 * a3 * a3 - 2 * a2 - 2 * sqrt(y1 * y1 - 4 * a0));
+  }
+
+  if (!std::isnan(D)) {
+    dts.push_back(-a3 / 4 + R / 2 + D / 2);
+    dts.push_back(-a3 / 4 + R / 2 - D / 2);
+  }
+  if (!std::isnan(E)) {
+    dts.push_back(-a3 / 4 - R / 2 + E / 2);
+    dts.push_back(-a3 / 4 - R / 2 - E / 2);
+  }
+
+  return dts;
+}
+
+int main(){
+  std::vector<double> root = cubic(1,2,6,4);
+  std::cout << "Roots of Equation: " << root.at(0) << std::endl;
+}
